bbd088b9366e26e94573b9c24a21eafdf4e13a98
Dipsii1 <nadifaryastia@gmail.com>
2025-12-27 16:42:33 +0700

    Setup Nodemon For Project

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +36,13 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const port = process.env.APP_PORT || 4000;
+const env = process.env.ENV_TYPE || 'production';
+
+app.listen(port, () => {
+  console.log(`Server running in ${env} mode on port ${port}`);
 });
 
 module.exports = app;
