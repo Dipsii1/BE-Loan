1d0be9ddaa14681a89bdd8cfede1329e504f97c0
Dipsii1 <nadifaryastia@gmail.com>
2026-02-07 13:37:30 +0700

    update for app.listen for app.js

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use((err, req, res, next) => {
 })
 
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log(`Server berjalan di port ${PORT}`)
-})
+// const PORT = process.env.PORT || 3000
+// app.listen(PORT, () => {
+//   console.log(`Server berjalan di port ${PORT}`)
+// })
